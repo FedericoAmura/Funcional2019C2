98da5c90889dfbd812524747df9980206b1fe44f
FedericoAmura <federicoamura@gmail.com>
2019-11-17 02:38:22 -0300

    Configuradas networks del cluster de entrenamiento y del sistema posta (filler, entrenador, api y db)

--- trainer/src/main/scala/sfpsfiuba/DB.scala
+++ trainer/src/main/scala/sfpsfiuba/DB.scala
@@ -13,7 +13,7 @@ object DB {
 
   val xa = Transactor.fromDriverManager[IO](
     "org.postgresql.Driver",     // driver classname
-    "jdbc:postgresql://host.docker.internal:54320/soy",     // connect URL (driver-specific)
+    "jdbc:postgresql://postgres:5432/soy",     // connect URL (driver-specific)
     "root",                  // user
     "root"                          // password
     // Blocker.liftExecutionContext(ExecutionContexts.synchronous) // just for testing
