79c9013e3f18554ef454a99bd64579c2286adf82
Seva Gavrilov <gavrilovseva@gmail.com>
2019-11-19 00:05:45 -0300

    Convierto dbFiller a "funcional"

---
+++ dbFiller/src/main/scala/sfpsfiuba/DB.scala
@@ -0,0 +1,28 @@
+package sfpsfiuba.commons
+
+import doobie._
+import doobie.implicits._
+import cats._
+import cats.effect._
+import cats.implicits._
+import doobie.util.ExecutionContexts
+
+object DB {
+
+  implicit val cs = IO.contextShift(ExecutionContexts.synchronous)
+
+  val xa = Transactor.fromDriverManager[IO](
+    "org.postgresql.Driver",     // driver classname
+    "jdbc:postgresql://postgres:5432/soy",     // connect URL (driver-specific)
+    "root",                  // user
+    "root"                          // password
+    // Blocker.liftExecutionContext(ExecutionContexts.synchronous) // just for testing
+  )
+
+  def insertRows(rows: List[Row]): IO[Int] = {
+    val query: String = "INSERT INTO soy VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    Update[Row](query, None)
+      .updateMany(rows)
+      .transact(xa)
+  }
+}
