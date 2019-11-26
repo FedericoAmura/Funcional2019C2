6c5bfe6bbdb86ecf339a5674057589d6cabb559a
Seva Gavrilov <gavrilovseva@gmail.com>
2019-11-26 15:45:02 -0300

    Paso a usar frameless en spark

--- trainer/src/main/scala/sfpsfiuba/ml/SparkRFPipeline.scala
+++ trainer/src/main/scala/sfpsfiuba/ml/SparkRFPipeline.scala
@@ -1,23 +1,52 @@
 package sfpsfiuba.ml
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.StructType
-import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.ml.regression.RandomForestRegressor
 import org.apache.spark.ml.{Pipeline, PipelineModel}
 import org.jpmml.sparkml.PMMLBuilder
-import java.io.File
+
+
+import frameless.functions._
+import frameless.functions.aggregate._
+import frameless.TypedDataset
+import frameless.syntax._
+import frameless.ml._
+import frameless.ml.feature._
+import frameless.ml.regression._
+import org.apache.spark.ml.linalg.Vector
 
 import sfpsfiuba.Row
 
+import java.io.File
+
 object SparkRFPipeline {
 
-  // case class TrainingRow(Open: Double, High: Double, Cierre: Double)
-  // case class Features(Open: Double, High: Double)
-  // case class DatasetWithFeatures(Open: Double, High: Double, Cierre: Double, features: Vector)
-  // case class RFInputs(Cierre: Double, features: Vector)
-  // case class RowPrediction(Open: Double, High: Double, Cierre: Double, features: Vector, prediction: Double)
+  case class TrainingRow(DolarBN: Double, DolarItau: Double, DifSem: Double, Cierre: Double)
+  case class Features(DolarBN: Double, DolarItau: Double, DifSem: Double)
+  case class DatasetWithFeatures(DolarBN: Double, DolarItau: Double, DifSem: Double, Cierre: Double, features: Vector)
+  case class RFInputs(Cierre: Double, features: Vector)
+  case class RowPrediction(DolarBN: Double, DolarItau: Double, DifSem: Double, Cierre: Double, features: Vector, prediction: Double)
+
+
+  class PipelineWrapper (
+    assembler: TypedVectorAssembler[Features],
+    model: TypedRandomForestRegressor[RFInputs]
+  ) {
+    val pipeline = new Pipeline()
+      .setStages(Array(assembler.transformer
+                        .setOutputCol("features"),  // Hack para que funcione pipeline de spark
+                      model.estimator))
+
+    def fit(ds: TypedDataset[TrainingRow]): PipelineModel = {
+      // Only for typechecking
+      val featuresDS: TypedDataset[DatasetWithFeatures] = assembler.transform(ds).as[DatasetWithFeatures]
+      val _ = model.fit(featuresDS)
+
+      pipeline.fit(ds.dataset)
+    }
+  }
+
 
   def run(rows: List[Row], modelPath: String): Unit = {
 
@@ -32,31 +61,31 @@ object SparkRFPipeline {
 
     import spark.implicits._
 
-    case class TrainingRow(Open: Double, High: Double, Cierre: Double)
+    val rowsDs: TypedDataset[Row] = TypedDataset.create(rows)
 
-    val df = rows.toDS().select("DolarBN", "DolarItau", "DifSem", "Cierre")
+    val trainingRowDs: TypedDataset[TrainingRow] = rowsDs.select(
+      rowsDs('DolarBN),
+      rowsDs('DolarItau),
+      rowsDs('DifSem),
+      rowsDs('Cierre)
+    ).as[TrainingRow]
 
-    val Array(testSet, trainingSet) = df.randomSplit(Array(0.20, 0.80))
+    val Array(testSet, trainingSet) = trainingRowDs.randomSplit(Array(0.20, 0.80))
 
-    trainingSet.show()
+    trainingSet.show().run()
 
-    val vectorAssembler: VectorAssembler = new VectorAssembler().
-      setInputCols(Array("DolarBN", "DolarItau", "DifSem")).
-      setOutputCol("features")
+    val assembler = TypedVectorAssembler[Features]
+    val rf = TypedRandomForestRegressor[RFInputs]
 
-    val rfRegressor: RandomForestRegressor = new RandomForestRegressor()
-      .setFeaturesCol("features")
-      .setLabelCol("Cierre")
-
-    val schema: StructType = trainingSet.schema;
-
-    val pipeline: Pipeline = new Pipeline()
-      .setStages(Array(vectorAssembler, rfRegressor))
-    val model: PipelineModel = pipeline.fit(trainingSet)
-
-    val predictions: DataFrame = model.transform(testSet)
+    
+    val pipelineWrapper = new PipelineWrapper(assembler, rf)
+    
+    val model: PipelineModel = pipelineWrapper.fit(trainingSet)
+    
+    val predictions = model.transform(testSet.dataset)
     predictions.show()
-
+    
+    val schema: StructType = trainingRowDs.dataset.schema;
     val builder: PMMLBuilder = new PMMLBuilder(schema, model)
 
     builder.buildFile(new File(modelPath))
