3ea0f1404ca63289929c41cf8e02d1b7022fc830
Seva Gavrilov <gavrilovseva@gmail.com>
2019-12-02 01:36:48 -0300

    Uso frameless y separo escritura a archivo en monada

--- trainer/src/main/scala/Main.scala
+++ trainer/src/main/scala/Main.scala
@@ -5,6 +5,7 @@ import org.apache.log4j.Level
 
 import sfpsfiuba.DB
 import sfpsfiuba.ml.SparkRFPipeline
+import sfpsfiuba.ml.PMMLWriter
 import sfpsfiuba.ml.RandomForestPMMLEvaluator
 
 object Main extends App {
@@ -14,7 +15,11 @@ object Main extends App {
 
   val modelPath: String = Properties.envOrElse("MODEL_OUTPUT_PATH", "/tmp/model.pmml")
 
-  val program: IO[Unit] = DB.getRows.map(rows => SparkRFPipeline.run(rows, modelPath))
+  val program: IO[Unit] = for {
+    rows <- DB.getRows
+    bytes <- SparkRFPipeline.run(rows)
+    _ <- PMMLWriter.writeBytes(bytes, modelPath)
+  } yield ()
 
   program.unsafeRunSync()
 
--- trainer/src/main/scala/sfpsfiuba/ml/SparkRFPipeline.scala
+++ trainer/src/main/scala/sfpsfiuba/ml/SparkRFPipeline.scala
@@ -3,9 +3,11 @@ package sfpsfiuba.ml
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.StructType
-import org.apache.spark.ml.{Pipeline, PipelineModel}
-import org.jpmml.sparkml.PMMLBuilder
+import org.apache.spark.ml.{Pipeline, PipelineModel, Transformer}
+import org.apache.spark.ml.regression.RandomForestRegressionModel
 
+import org.jpmml.sparkml.PMMLBuilder
+import org.jpmml.model.MetroJAXBUtil
 
 import frameless.functions._
 import frameless.functions.aggregate._
@@ -18,7 +20,8 @@ import org.apache.spark.ml.linalg.Vector
 
 import sfpsfiuba.Row
 
-import java.io.File
+import cats.effect.{IO, Resource}
+
 
 object SparkRFPipeline {
 
@@ -28,36 +31,7 @@ object SparkRFPipeline {
   case class RFInputs(Cierre: Double, features: Vector)
   case class RowPrediction(DolarBN: Double, DolarItau: Double, DifSem: Double, Cierre: Double, features: Vector, prediction: Double)
 
-
-  class PipelineWrapper (
-    assembler: TypedVectorAssembler[Features],
-    model: TypedRandomForestRegressor[RFInputs]
-  ) {
-    val pipeline = new Pipeline()
-      .setStages(Array(assembler.transformer
-                        .setOutputCol("features"),  // Hack para que funcione pipeline de spark
-                      model.estimator))
-
-    def fit(ds: TypedDataset[TrainingRow]): PipelineModel = {
-      // Only for typechecking
-      val featuresDS: TypedDataset[DatasetWithFeatures] = assembler.transform(ds).as[DatasetWithFeatures]
-      val _ = model.fit(featuresDS)
-
-      pipeline.fit(ds.dataset)
-    }
-  }
-
-
-  def run(rows: List[Row], modelPath: String): Unit = {
-
-    val conf: SparkConf = new SparkConf()
-      .setAppName("trainer")
-      .setMaster("local")
-      .set("spark.ui.enabled", "false")
-      .set("spark.executor.memory", "2g")
-      .set("spark.driver.memory", "2g")
-
-    implicit val spark: SparkSession = SparkSession.builder().config(conf).appName("trainer").getOrCreate()
+  def runOnSparkSession(rows: List[Row])(implicit spark: SparkSession): Array[Byte] = {
 
     import spark.implicits._
 
@@ -70,26 +44,49 @@ object SparkRFPipeline {
       rowsDs('Cierre)
     ).as[TrainingRow]
 
-    val Array(testSet, trainingSet) = trainingRowDs.randomSplit(Array(0.20, 0.80))
+    trainingRowDs.show().run()  // TODO: remove
 
-    trainingSet.show().run()
+    val Array(testSet, trainingSet) = trainingRowDs.randomSplit(Array(0.20, 0.80))
 
     val assembler = TypedVectorAssembler[Features]
     val rf = TypedRandomForestRegressor[RFInputs]
 
+    val featuresDS: TypedDataset[DatasetWithFeatures] = assembler.transform(trainingSet).as[DatasetWithFeatures]
+
+    val model: AppendTransformer[_, _, RandomForestRegressionModel] = rf.fit(featuresDS).run()
+
+    val pipeline = new Pipeline()
+      .setStages(Array(assembler.transformer
+                        .setOutputCol("features"),  // Hack para que funcione pipeline de spark
+                       model.transformer))
     
-    val pipelineWrapper = new PipelineWrapper(assembler, rf)
-    
-    val model: PipelineModel = pipelineWrapper.fit(trainingSet)
-    
-    val predictions = model.transform(testSet.dataset)
-    predictions.show()
-    
+    // Hack
+    val pipelineModel: PipelineModel = pipeline.fit(trainingRowDs.dataset)
+
     val schema: StructType = trainingRowDs.dataset.schema;
-    val builder: PMMLBuilder = new PMMLBuilder(schema, model)
+    // val builder: PMMLBuilder = new PMMLBuilder(schema, pipelineModel)
 
-    builder.buildFile(new File(modelPath))
+    val builder = new PMMLBuilder(schema, pipelineModel)
 
-    spark.stop()
+    builder.buildByteArray()
+  }
+
+  def run(rows: List[Row]): IO[Array[Byte]] = {
+
+    Resource.fromAutoCloseable(IO(
+      SparkSession.builder()
+          .config("spark.master", "local")
+          .config("spark.ui.enabled", "false")
+          .config("spark.executor.memory", "2g")
+          .config("spark.driver.memory", "2g")
+          .appName("trainer")
+          .getOrCreate()
+      ) 
+    )
+    .use {
+      spark => IO {
+        runOnSparkSession(rows)(spark)
+      }
+    }
   }
 }
