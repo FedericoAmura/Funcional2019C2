46e38efd4034b4773d3737d71cd22a09e6c47391
Seva Gavrilov <gavrilovseva@gmail.com>
2019-12-04 10:33:58 -0300

    Edito informe

--- script/generarInformes.sh
+++ script/generarInformes.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-pandoc preambulo.md informe.md --pdf-engine=xelatex -o informe.pdf
+pandoc preambulo.md informe.md --pdf-engine=xelatex -o informe.pdf --highlight-style tango
 pandoc preambulo.md informe.md -s -o informe.tex
